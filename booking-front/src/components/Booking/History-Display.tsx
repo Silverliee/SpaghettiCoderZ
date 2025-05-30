739ab44874bdf6c81de650f7dec2d7b8527760fe
Mulet-J <71126090+Mulet-J@users.noreply.github.com>
2025-05-31 00:10:48 +0200

    Replace alert by sonner toaster

--- booking-front/src/components/Booking/History-Display.tsx
+++ booking-front/src/components/Booking/History-Display.tsx
@@ -5,87 +5,88 @@ import BookingService from "@/services/bookingService";
 import { BookingStatus } from "@/interface/interface";
 import { Card, CardContent } from "../ui/card";
 import HistoryBookingItem from "./history-booking-item";
+import { toast } from "sonner";
 
 export default function HistoryDisplay() {
-	const [bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState([]);
 
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	const fakeBookings = [
-		{ id: "1", slotId: "A1", date: "2023-10-01T00:00:00", status: 3 },
-		{ id: "2", slotId: "A2", date: "2023-10-02T00:00:00", status: 1 },
-		{ id: "3", slotId: "A3", date: "2023-10-03T00:00:00", status: 2 },
-		{ id: "4", slotId: "A3", date: "2023-10-04T00:00:00", status: 0 },
-		{ id: "5", slotId: "A1", date: "2026-10-01T00:00:00", status: 3 },
-		{ id: "6", slotId: "A2", date: "2026-10-02T00:00:00", status: 1 },
-		{ id: "7", slotId: "A3", date: "2026-10-03T00:00:00", status: 2 },
-		{ id: "8", slotId: "A3", date: "2026-10-04T00:00:00", status: 0 },
-	];
+  const fakeBookings = [
+    { id: "1", slotId: "A1", date: "2023-10-01T00:00:00", status: 3 },
+    { id: "2", slotId: "A2", date: "2023-10-02T00:00:00", status: 1 },
+    { id: "3", slotId: "A3", date: "2023-10-03T00:00:00", status: 2 },
+    { id: "4", slotId: "A3", date: "2023-10-04T00:00:00", status: 0 },
+    { id: "5", slotId: "A1", date: "2026-10-01T00:00:00", status: 3 },
+    { id: "6", slotId: "A2", date: "2026-10-02T00:00:00", status: 1 },
+    { id: "7", slotId: "A3", date: "2026-10-03T00:00:00", status: 2 },
+    { id: "8", slotId: "A3", date: "2026-10-04T00:00:00", status: 0 },
+  ];
 
-	useEffect(() => {
-		if (user && user.userId && bookings.length === 0) {
-			BookingService.getBookingsByUserId(user.userId)
-				.then((response) => {
-					setBookings(response);
-				})
-				.catch((error) => {
-					console.error("Failed to fetch bookings:", error);
-					setBookings(fakeBookings); // Use fake data for demonstration
-				});
-		}
-	}, [user]);
+  useEffect(() => {
+    if (user && user.userId && bookings.length === 0) {
+      BookingService.getBookingsByUserId(user.userId)
+        .then((response) => {
+          setBookings(response);
+        })
+        .catch((error) => {
+          console.error("Failed to fetch bookings:", error);
+          setBookings(fakeBookings); // Use fake data for demonstration
+        });
+    }
+  }, [user]);
 
-	const today = new Date();
-	today.setHours(0, 0, 0, 0);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-	const futureBookings = bookings.filter((b) => new Date(b.date) >= today);
-	const pastBookings = bookings.filter((b) => new Date(b.date) < today);
+  const futureBookings = bookings.filter((b) => new Date(b.date) >= today);
+  const pastBookings = bookings.filter((b) => new Date(b.date) < today);
 
-	const handleCancelBooking = (bookingId: string) => {
-		BookingService.deleteBooking(bookingId)
-			.then(() => {
-				setBookings((prevBookings) =>
-					prevBookings.map((booking) =>
-						booking.id === bookingId
-							? { ...booking, status: BookingStatus.CANCELLED }
-							: booking
-					)
-				);
-			})
-			.catch((error) => {
-				console.error("Failed to cancel booking:", error);
-				alert("Erreur lors de l'annulation de la réservation.");
-				setBookings((prevBookings) =>
-					prevBookings.map((booking) =>
-						booking.id === bookingId
-							? { ...booking, status: BookingStatus.CANCELLED }
-							: booking
-					)
-				);
-			});
-	};
+  const handleCancelBooking = (bookingId: string) => {
+    BookingService.deleteBooking(bookingId)
+      .then(() => {
+        setBookings((prevBookings) =>
+          prevBookings.map((booking) =>
+            booking.id === bookingId
+              ? { ...booking, status: BookingStatus.CANCELLED }
+              : booking
+          )
+        );
+      })
+      .catch((error) => {
+        console.error("Failed to cancel booking:", error);
+        toast.error("Erreur lors de l'annulation de la réservation.");
+        setBookings((prevBookings) =>
+          prevBookings.map((booking) =>
+            booking.id === bookingId
+              ? { ...booking, status: BookingStatus.CANCELLED }
+              : booking
+          )
+        );
+      });
+  };
 
-	const renderSection = (title: string, list: typeof bookings) => (
-		<div className="mb-6">
-			<h3 className="text-xl font-semibold mb-4 text-gray-700">{title}</h3>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				{list.map((booking) => {
-					return (
-						<HistoryBookingItem
-							booking={booking}
-							onCancel={handleCancelBooking}
-						/>
-					);
-				})}
-			</div>
-		</div>
-	);
+  const renderSection = (title: string, list: typeof bookings) => (
+    <div className="mb-6">
+      <h3 className="text-xl font-semibold mb-4 text-gray-700">{title}</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {list.map((booking) => {
+          return (
+            <HistoryBookingItem
+              booking={booking}
+              onCancel={handleCancelBooking}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 
-	return (
-		<div className="p-6 max-w-6xl mx-auto">
-			<h2 className="text-3xl font-bold mb-8">Historique de Réservation</h2>
-			{renderSection("Réservations à venir", futureBookings)}
-			{renderSection("Réservations passées", pastBookings)}
-		</div>
-	);
+  return (
+    <div className="p-6 max-w-6xl mx-auto">
+      <h2 className="text-3xl font-bold mb-8">Historique de Réservation</h2>
+      {renderSection("Réservations à venir", futureBookings)}
+      {renderSection("Réservations passées", pastBookings)}
+    </div>
+  );
 }
--- booking-front/src/main.tsx
+++ booking-front/src/main.tsx
@@ -4,13 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<AuthProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</AuthProvider>
-	</StrictMode>
+  <StrictMode>
+    <Toaster />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
 );
